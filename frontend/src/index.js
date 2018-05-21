0b979f98babde4df210dc8d032eef8c2cc8495bd
P. Doyle <patrick.doyle126@gmail.com>
2018-05-20 19:21:22 -0500

    adding metamask check

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Import Web3 - To connect to blockchain
+import {Web3Provider} from 'react-web3';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Web3Provider> <App /> </Web3Provider> , document.getElementById('root'));
 registerServiceWorker();

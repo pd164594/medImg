9199edd6e72f98ecbe57a1c4a217eb12261daaa7
P. Doyle <patrick.doyle126@gmail.com>
2018-05-29 22:54:43 -0500

    adding smart contract

---
+++ smart_contract/erc20.sol
@@ -0,0 +1,10 @@
+ Volume in drive C is OS
+ Volume Serial Number is 824E-EEA2
+
+ Directory of C:\Users\Pat Doyle\Documents\Projects\devannotation\smart_contract
+
+05/28/2018  04:53 PM    <DIR>          .
+05/28/2018  04:53 PM    <DIR>          ..
+05/28/2018  04:53 PM                 0 erc20.sol
+               1 File(s)              0 bytes
+               2 Dir(s)  1,816,531,304,448 bytes free
